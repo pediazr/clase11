fb42658011edb78f8939c7925f50d58e206c53c7
Pedro Díaz Rivera <pedro.neox@gmail.com>
2023-03-29 22:35:48 -0300

    ejercicio clase 11

---
+++ index.js
@@ -0,0 +1,21 @@
+//calcular IMC (indice de masa corporal) de una persona.//
+var peso, altura, imc;
+
+peso = 65; /*Kg*/
+altura = 1.75; /*mt*/
+imc = peso / (altura * altura);
+console.log(imc);
+
+//calcular IVA de un monto de compra//
+var monto;
+const iva = monto * 0.19;
+
+monto = 23990;
+console.log(iva);
+
+//determinar la hipotenusa dada los catetos de un triangulo//
+var cateto1, cateto2;
+const hipo = Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+cateto1 = 3;
+cateto2 = 4;
+console.log(hipo);
